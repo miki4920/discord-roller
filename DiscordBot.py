ce3deb827377b411713672e992508af811e06ba8
Mikolaj Grobelny <miki4920@gmail.com>
2020-05-28 16:48:30 +0100

    Changes to GM Roll TODO: Create Error Handling

--- DiscordBot.py
+++ DiscordBot.py
@@ -19,25 +19,26 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('!'):
-        message_start = message.content[0:3]
+        message_start = message.content[0:3].lower()
         message_content = message.content[3:]
         result, dice_rolls = roller.roll_dice(message_content)
         if "gr" in message_start:
             for member in server_members:
                 for role in member.roles:
-                    if role.name == "GM":
+                    if role.name == "DM":
                         await member.send(f"{result}\n"
                                           f"Details: {message_content} {dice_rolls}")
                         break
             else:
-                await message.channel.send("There is nobody who has a rank of 'GM' on your server")
-        elif "pr" in message_content:
+                await message.channel.send("There is nobody who has a rank of 'DM' on your server")
+        elif "pr" in message_start:
             await message.author.send(f"{result}\n"
                                       f"Details: {message_content} {dice_rolls}")
-        elif "r" in message_content:
-
+        elif "r" in message_start:
             await message.channel.send(f"{result}\n"
                                        f"Details: {message_content} {dice_rolls}")
+        elif "h" in message_start:
+            await message.channel.send("Help is in progress!")
 
 
 client.run(token)
