757545355c20008aff3d0b2d6270888cd77347b2
Mikolaj Grobelny <miki4920@gmail.com>
2020-05-31 13:39:54 +0100

    Fixed Token and Added error handling

--- DiscordBot.py
+++ DiscordBot.py
@@ -5,9 +5,9 @@ from Roller import DiceRoll
 link = "https://github.com/miki4920/discord-roller/blob/master/ReadMe.md"
 client = discord.Client()
 roller = DiceRoll()
-token = os.getenv("TOKEN")
-code_dictionary = {"h ": 0,
-                   "r ": 1,
+token= os.getenv("TOKEN")
+code_dictionary = {"h": 0,
+                   "r": 1,
                    "pr": 2,
                    "gr": 3}
 
@@ -22,12 +22,23 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('!'):
-        message_code = code_dictionary[message.content[1:3]]
+        message_start = message.content[1:3].replace(" ", "").lower()
+        try:
+            message_code = code_dictionary[message_start]
+        except KeyError:
+            await message.channel.send(f"The command you are trying to use does not exist.\n"
+                                       f"Please type '!h' for further information.")
+            return
         if message_code == 0:
             await message.author.send(f"The manual is located here: {link}")
         if message_code > 0:
-            result, dice_rolls = roller.roll_dice(message.content[3:])
-            return_message = f"{result}\nDetails: {message.content[3:]}\n{dice_rolls}"
+            try:
+                result, dice_rolls = roller.roll_dice(message.content[3:])
+                return_message = f"{result}\nDetails: {message.content[3:]}\n{dice_rolls}"
+            except ValueError:
+                await message.channel.send(f"The command you tried to use: '{message.content}' is incorrect.\n"
+                                           f"Please type '!h' for further information.")
+                return
             if message_code == 1:
                 await message.channel.send(return_message)
             elif message_code == 2:
@@ -40,7 +51,7 @@ async def on_message(message):
                             await member.send(return_message)
                             return
                 else:
-                    await message.channel.send("There is nobody who has a rank of 'DM' on your server")
+                    await message.channel.send("There is nobody who has a rank of 'DM' on your server.")
 
 
 client.run(token)
