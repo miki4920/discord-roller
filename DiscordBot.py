4e70227fa0597899cfbfcb1a6b32294d6aececbf
Mikolaj Grobelny <miki4920@gmail.com>
2020-09-07 15:20:42 +0100

    Implemented Lower Case to get partial matches TODO: Add choice option when there are multiple spells to pick from

--- DiscordBot.py
+++ DiscordBot.py
@@ -14,7 +14,7 @@ roller = DiceRoll()
 downtime = DowntimeScheduler()
 wildmagic = WildMagic()
 reference = ReferenceHandler(api)
-token = os.getenv("TOKEN")
+token="NzEzNzM0MzUxNjQ4NTg3Nzc2.Xska6Q.388qGhkn6h42iPi8BrbXVgM5x_I"
 
 
 code_dictionary = {"h": 0,
--- ReferenceHandler.py
+++ ReferenceHandler.py
@@ -15,7 +15,7 @@ class ReferenceHandler(object):
             return item_name
         for item_part in item_name.split(" "):
             for item in item_list:
-                if item_part in item:
+                if item_part in item.lower():
                     return item
 
         closest_item_names = get_close_matches(item_name, item_list, cutoff=0.6)
