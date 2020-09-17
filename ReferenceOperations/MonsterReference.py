8be3c4d9ffe0509f2cd937c4181489ecc6bd375f
Mikolaj Grobelny <miki4920@gmail.com>
2020-09-17 11:02:44 +0100

    Added entire stat block, and special abilities TODO: Actions, Legendary Actions, Spellcasting

--- ReferenceOperations/MonsterReference.py
+++ ReferenceOperations/MonsterReference.py
@@ -1,9 +1,10 @@
 from math import floor
+from FileHandler import read_pickle
 
 
 def ability_conversion(ability_score):
     ability_score = str(floor(int(ability_score) / 2) - 5)
-    if int(ability_score) > 0:
+    if int(ability_score) > -1:
         ability_score = "+" + ability_score
 
     return ability_score
@@ -51,9 +52,56 @@ def get_proficiencies(proficiencies):
         else:
             skills.append(f"{proficiency['name'][7:]}: {value}")
     if saving_throws:
-        return_string += "**Saving Throws:** " + " ".join(saving_throws) + "\n"
+        return_string += "**Saving Throws:** " + ", ".join(saving_throws) + "\n"
     if skills:
-        return_string += "**Skills:** " + " ".join(skills) + "\n"
+        return_string += "**Skills:** " + ", ".join(skills) + "\n"
+    return return_string
+
+
+def get_senses(senses):
+    return_string = "**Senses:** "
+    for sense in senses:
+        return_string += f"{sense.capitalize().replace('_', ' ')}: {senses[sense]}, "
+    return return_string[:-2] + "\n"
+
+
+def get_damage_conditions(damage_conditions):
+    return_string = ""
+    vulnerabilities, resistances, immunities, conditions = damage_conditions
+    if vulnerabilities:
+        return_string += "**Vulnerabilities:** " + ", ".join(vulnerabilities) + "\n"
+    if resistances:
+        return_string += "**Resistances:** " + ", ".join(resistances) + "\n"
+    if immunities:
+        return_string += "**Immunities:** " + ", ".join(immunities) + "\n"
+    if conditions:
+        conditions = [condition["name"] for condition in conditions]
+        return_string += "**Conditions Immunities:** " + ", ".join(conditions) + "\n"
+    return return_string
+
+
+def get_languages(languages):
+    if languages:
+        return "**Languages:** " + languages + "\n"
+    return ""
+
+
+def get_xp(challenge_rating):
+    xp = read_pickle("FileStorage/XPList.pickle")[str(challenge_rating)]
+    return f"**CR**: {challenge_rating} ({xp} XP)" + "\n\n"
+
+
+def get_special_abilities(special_abilities):
+    return_string = ""
+    if special_abilities:
+        return_string = "**Special Abilities:**\n"
+        for special_ability in special_abilities:
+            return_string += f"**{special_ability['name']}"
+            usage = special_ability.get("usage")
+            if usage:
+                return_string += f" ({usage['times']}/{usage['type'].capitalize()}):** {special_ability['desc']}\n\n"
+            else:
+                return_string += f"** {special_ability['desc']}\n\n"
     return return_string
 
 
@@ -67,13 +115,22 @@ def monster_reference(monster_json):
     hit_points_calculations = "(" + str(get_hit_points_calculation(monster_json.get("hit_dice"), monster_json.get("constitution"))) + ")\n"
     speed = get_speed(monster_json.get("speed"))
     ability_scores_string = get_ability_scores(monster_json)
-    proficiencies = monster_json.get("proficiencies")
-    proficiencies = get_proficiencies(proficiencies)
-    # TODO Add Proficiencies in Skills
+    proficiencies = get_proficiencies(monster_json.get("proficiencies"))
+    senses = get_senses(monster_json.get("senses"))
+    damage_conditions = get_damage_conditions((monster_json.get("damage_vulnerabilities"), monster_json.get("damage_resistances"), monster_json.get("damage_immunities"), monster_json.get("condition_immunities")))
+    languages = get_languages(monster_json.get("languages"))
+    xp = get_xp(monster_json.get("challenge_rating"))
+    special_abilities = get_special_abilities(monster_json.get("special_abilities"))
     return_string = f"*{size} {monster_type}, {alignment}" \
                     f"**AC**: {armor_class}" \
                     f"**HP**: {hit_points_average} {hit_points_calculations}" \
                     f"**Speed**: {speed}" \
                     f"{ability_scores_string}" \
-                    f"{proficiencies}"
+                    f"{proficiencies}" \
+                    f"{senses}" \
+                    f"{damage_conditions}" \
+                    f"{languages}" \
+                    f"{xp}" \
+                    f"{special_abilities}"
+
     return name, return_string
