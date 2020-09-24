a1bb3f3064ee5cb90dae83bb005f960061e8fcf6
Mikolaj Grobelny <miki4920@gmail.com>
2020-09-24 11:33:37 +0100

    Implemented Hit Dice reference and added error raising for the test mode TODO: Proficiencies

--- ReferenceOperations/ClassReference.py
+++ ReferenceOperations/ClassReference.py
@@ -1,4 +1,5 @@
 from UtilityHandler import read_json
+from math import ceil
 
 
 def get_levels(class_json):
@@ -16,13 +17,26 @@ def get_formatted_level_features(class_json):
     levels = get_levels(class_json)
     for level in levels:
         formatted_level_features += f"`{level}` {', '.join(levels[level])}\n"
-    return formatted_level_features
+    return formatted_level_features + "\n"
+
+
+def get_hit_points(class_json):
+    name = class_json.get("name").lower()
+    hit_die_value = class_json.get("hit_die")
+    hit_die = "1d" + str(hit_die_value)
+    hit_die_average = ceil((1+hit_die_value)/2)
+    hit_points = f"\n**Hit Dice: ** {hit_die} per {name} level\n" \
+                 f"**Hit Points at 1st Level: **{hit_die_value} + your Constitution modifier\n" \
+                 f"**Hit Points at Higher Levels: **{hit_die} (or {hit_die_average}) + your Constitution modifier per {name} level after 1st\n"
+    return hit_points + "\n"
 
 
 def class_reference(class_json):
     messages = []
     name = class_json.get("name")
     levels = get_formatted_level_features(class_json)
-    class_block = f"{levels}"
+    hit_points = get_hit_points(class_json)
+    class_block = f"{levels}" \
+                  f"**Hit Points** {hit_points}"
     messages.append((name, class_block))
     return messages
