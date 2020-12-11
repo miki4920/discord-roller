9c693c4d3991ab486d2d777ae06ec8bdb52f0ca9
Mikolaj Grobelny <miki4920@gmail.com>
2020-12-11 16:54:03 +0000

    Structurised files TODO: Decide the file storage system and transfer the whole project to it.

--- Utility/GetHelp.py
+++ Utility/GetHelp.py
@@ -2,6 +2,6 @@ from Utility.UtilityHandler import read_pickle
 
 
 def get_help_messages():
-    commands_help = ["How to Tame Marduk", read_pickle("../FileStorage/PickleFiles/CommandHelp.pickle")]
+    commands_help = ["How to Tame Marduk", read_pickle("../FileStorage/TextFiles/CommandHelp.pickle")]
     messages = [commands_help]
     return messages
--- WildMagicHandler.py
+++ WildMagicHandler.py
@@ -3,7 +3,7 @@ from Utility.UtilityHandler import read_pickle
 
 class WildMagic(object):
     def __init__(self):
-        self.wild_magic_dictionary = read_pickle("FileStorage/PickleFiles/WildMagicDictionary.pickle")
+        self.wild_magic_dictionary = read_pickle("FileStorage/ReferenceLists/WildMagicDictionary.pickle")
 
     def determine_wild_magic(self, roll):
         roll = [(roll, roll + 1) if roll % 2 == 1 else (roll - 1, roll)][0]
