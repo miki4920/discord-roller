c0a1524659335674a20774198134304af6214292
Nicolas Grobelny <miki4920@gmail.com>
2021-08-22 15:26:26 +0100

    Added new error types to handle user not inserting any command arguments.

--- Utility/ErrorHandler.py
+++ Utility/ErrorHandler.py
@@ -1,60 +1,64 @@
 class RollerException(Exception):
-    def __init__(self, command, message):
-        self.command = command
+    def __init__(self, message):
         self.message = message
 
     def __str__(self):
-        return f"{self.message}\nYour Command: {self.command}"
+        return f"{self.message}"
 
 
 def command_not_existing():
     message = "The command you tried to use does not exist.\nPlease type **!h** for more information."
-    return RollerException("", message)
+    return RollerException(message)
 
 
 def die_too_low_for_explosion():
     message = "You must roll a d2 or higher to roll exploding dice."
-    return RollerException("", message)
+    return RollerException(message)
 
 
 def division_by_zero_error():
     message = "At one point, you tried to divide by 0. \nPlease make sure your roll is mathematically correct"
-    return RollerException("", message)
+    return RollerException(message)
 
 
 def drop_keep_modifier_too_high():
     message = "You cannot drop/keep more dice than you roll."
-    return RollerException("", message)
+    return RollerException(message)
 
 
 def roll_is_zero():
     message = "You cannot roll a 0 sided die."
-    return RollerException("", message)
+    return RollerException(message)
 
 
 def unexpected_error(error):
     message = "A Marduk has encountered an unexpected error: " + error + "\nPlease copy this whole message and send it " \
                                                                          "to our support server so we may improve " \
                                                                          "this bot. "
-    return RollerException("", message)
+    return RollerException(message)
+
+
+def too_few_arguments():
+    message = "You must provide at least one provide at least one value for this command."
+    return RollerException(message)
 
 
 def too_high_level():
     message = "You are trying to reference a level outside of 1-20 range."
-    return RollerException("", message)
+    return RollerException(message)
 
 
 def too_many_dice():
     message = "You have used too many dice.\nThis bot has a limit of 2000 characters.\nThat is equals approximately " \
               "400d20. "
-    return RollerException("", message)
+    return RollerException(message)
 
 
 def too_many_operators():
     message = "You have used too many operators, or maybe you have forgotten a bracket."
-    return RollerException("", message)
+    return RollerException(message)
 
 
 def wrong_command_format():
     message = "Your command is in wrong format.\nPlease refer to **!h** for more information."
-    return RollerException("", message)
+    return RollerException(message)
