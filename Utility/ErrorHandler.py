b70ed9f12cd258fb8794cded1295dd0806e659fb
Nicolas Grobelny <miki4920@gmail.com>
2021-08-23 19:41:31 +0100

    Updated errors to use classes rather than callable functions.

--- Utility/ErrorHandler.py
+++ Utility/ErrorHandler.py
@@ -6,59 +6,64 @@ class RollerException(Exception):
         return f"{self.message}"
 
 
-def command_not_existing():
-    message = "The command you tried to use does not exist.\nPlease type **!h** for more information."
-    return RollerException(message)
+class DieTooLowForExplosion(RollerException):
+    def __init__(self):
+        message = "You must roll a d2 or higher to roll exploding dice."
+        super().__init__(message)
 
 
-def die_too_low_for_explosion():
-    message = "You must roll a d2 or higher to roll exploding dice."
-    return RollerException(message)
+class DivisionByZeroError(RollerException):
+    def __init__(self):
+        message = "At one point, you tried to divide by 0. \nPlease make sure your roll is mathematically correct"
+        super().__init__(message)
 
 
-def division_by_zero_error():
-    message = "At one point, you tried to divide by 0. \nPlease make sure your roll is mathematically correct"
-    return RollerException(message)
+class DropKeepModifierTooHigh(RollerException):
+    def __init__(self):
+        message = "You cannot drop/keep more dice than you roll."
+        super().__init__(message)
 
 
-def drop_keep_modifier_too_high():
-    message = "You cannot drop/keep more dice than you roll."
-    return RollerException(message)
+class RollZero(RollerException):
+    def __init__(self):
+        message = "You cannot roll a 0 sided die."
+        super().__init__(message)
 
 
-def roll_is_zero():
-    message = "You cannot roll a 0 sided die."
-    return RollerException(message)
+class UnexpectedError(RollerException):
+    def __init__(self, error):
+        message = "A Marduk has encountered an unexpected error: " + error + "\nPlease copy this whole message and send it " \
+                                                                             "to our support server so we may improve " \
+                                                                             "this bot. "
+        super().__init__(message)
 
 
-def unexpected_error(error):
-    message = "A Marduk has encountered an unexpected error: " + error + "\nPlease copy this whole message and send it " \
-                                                                         "to our support server so we may improve " \
-                                                                         "this bot. "
-    return RollerException(message)
+class TooFewArguments(RollerException):
+    def __init__(self):
+        message = "You must provide at least one provide at least one value for this command."
+        super().__init__(message)
 
 
-def too_few_arguments():
-    message = "You must provide at least one provide at least one value for this command."
-    return RollerException(message)
+class TooHighLevel(RollerException):
+    def __init__(self):
+        message = "You are trying to reference a level outside of 1-20 range."
+        super().__init__(message)
 
 
-def too_high_level():
-    message = "You are trying to reference a level outside of 1-20 range."
-    return RollerException(message)
+class TooManyDice(RollerException):
+    def __init__(self):
+        message = "You have used too many dice.\nThis bot has a limit of 2000 characters.\nThat is equals approximately " \
+                  "400d20. "
+        super().__init__(message)
 
 
-def too_many_dice():
-    message = "You have used too many dice.\nThis bot has a limit of 2000 characters.\nThat is equals approximately " \
-              "400d20. "
-    return RollerException(message)
+class TooManyOperators(RollerException):
+    def __init__(self):
+        message = "You have used too many operators, or maybe you have forgotten a bracket."
+        super().__init__(message)
 
 
-def too_many_operators():
-    message = "You have used too many operators, or maybe you have forgotten a bracket."
-    return RollerException(message)
-
-
-def wrong_command_format():
-    message = "Your command is in wrong format.\nPlease refer to **!h** for more information."
-    return RollerException(message)
+class WrongCommandFormat(RollerException):
+    def __init__(self):
+        message = "Your command is in wrong format.\nPlease refer to **!help-me** or **/help-me** for more information."
+        super().__init__(message)
