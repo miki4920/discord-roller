c9efbfd487ed56630501a8e3343f5caf430069f8
M Grobelny <366-mg448@users.noreply.campus.cs.le.ac.uk>
2023-04-08 23:45:21 +0100

    Simplified get_help to a single embed

--- Utility/GetHelp.py
+++ Utility/GetHelp.py
@@ -3,5 +3,4 @@ from Utility.UtilityHandler import read_json
 
 def get_help_messages():
     commands_help = ["How to Tame Marduk", read_json("./FileStorage/ReferenceLists/CommandHelp.json")]
-    messages = [commands_help]
-    return messages
+    return commands_help
